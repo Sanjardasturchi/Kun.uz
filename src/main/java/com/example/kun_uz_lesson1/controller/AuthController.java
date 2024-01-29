c5bb1004a7bc9db7e4d81bef5752ef3c9f8d302a
Sanjar644 <sanjardasturchi@gmail.com>
2024-01-29 18:52:02 +0500

    auth.http done

--- src/main/java/com/example/kun_uz_lesson1/controller/AuthController.java
+++ src/main/java/com/example/kun_uz_lesson1/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.example.kun_uz_lesson1.controller;
 
 import com.example.kun_uz_lesson1.dto.AuthDTO;
 import com.example.kun_uz_lesson1.dto.ProfileDTO;
+import com.example.kun_uz_lesson1.dto.RegistrationDTO;
 import com.example.kun_uz_lesson1.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +10,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+// 1.  Authorization
+//       1. Registration (only USER)
+//            (name,surname,login/email/phone,password) + validation. password with MD5 encode.
+//       2. Login
+//            (email/phone,password)
+//       3. Verification
+//            (phone -> code) or (mail -> link)
+//            (should by limit for verification time)
 @RestController
 @RequestMapping("/auth")
 public class AuthController
@@ -17,9 +25,12 @@ public class AuthController
     @Autowired
     private AuthService authService;
 
+
+
     @PostMapping("/login")
     public ResponseEntity<ProfileDTO> login(@RequestBody AuthDTO authDTO){
         return ResponseEntity.ok(authService.auth(authDTO));
     }
+
 
 }
--- src/main/java/com/example/kun_uz_lesson1/service/AuthService.java
+++ src/main/java/com/example/kun_uz_lesson1/service/AuthService.java
@@ -2,7 +2,10 @@ package com.example.kun_uz_lesson1.service;
 
 import com.example.kun_uz_lesson1.dto.AuthDTO;
 import com.example.kun_uz_lesson1.dto.ProfileDTO;
+import com.example.kun_uz_lesson1.dto.RegistrationDTO;
 import com.example.kun_uz_lesson1.entity.ProfileEntity;
+import com.example.kun_uz_lesson1.enums.ProfileRole;
+import com.example.kun_uz_lesson1.enums.ProfileStatus;
 import com.example.kun_uz_lesson1.exp.AppBadException;
 import com.example.kun_uz_lesson1.repository.ProfileRepository;
 import com.example.kun_uz_lesson1.util.JWTUtil;
@@ -10,12 +13,15 @@ import com.example.kun_uz_lesson1.util.MD5Util;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.Random;
 
 @Service
 public class AuthService {
     @Autowired
     private ProfileRepository profileRepository;
+
 
     public ProfileDTO auth(AuthDTO authDTO) {
         Optional<ProfileEntity> entityOptional = profileRepository.findByEmailAndPassword(authDTO.getEmail(), MD5Util.encode(authDTO.getPassword()));
@@ -30,4 +36,51 @@ public class AuthService {
         profile.setJwt(JWTUtil.encode(entity.getId(),entity.getRole()));
         return profile;
     }
+
+//    public void registration(RegistrationDTO registration) {
+//        if (registration.getName() == null) {
+//            throw new AppBadException("Name is null");
+//        }
+//        if (registration.getSurname() == null) {
+//            throw new AppBadException("Surname is null");
+//        }
+//        if (registration.getEmail() == null) {
+//            throw new AppBadException("Email is null");
+//        }
+//        if (registration.getPassword() == null) {
+//            throw new AppBadException("Password is null");
+//        }
+//        if (registration.getValidation() == null) {
+//            throw new AppBadException("Validation is null");
+//        }
+//        if (!registration.getPassword().equals(registration.getValidation())){
+//            throw new AppBadException("Passwords not matches");
+//        }
+//        Optional<ProfileEntity> profile = profileRepository.getByEmail(registration.getEmail());
+//        if (profile.isPresent()) {
+//            ProfileEntity entity = profile.get();
+//            if (!entity.getStatus().equals(ProfileStatus.NONACTIVE)) {
+//                throw new AppBadException("Profile already exist");
+//            }
+//        }
+//        ProfileEntity entity=new ProfileEntity();
+//        entity.setName(registration.getName());
+//        entity.setSurname(registration.getSurname());
+//        entity.setPassword(MD5Util.encode(registration.getPassword()));
+//        entity.setEmail(registration.getEmail());
+//        entity.setVisible(true);
+//        entity.setRole(ProfileRole.USER);
+//        entity.setCreatedDate(LocalDateTime.now());
+//        entity.setStatus(ProfileStatus.NONACTIVE);
+//        LocalDateTime localDateTime=LocalDateTime.now();
+//        entity.setCodeEndTime(localDateTime.plusMinutes(1));
+//        Random random = new Random();
+//        int code=random.nextInt(1,9);
+//        for (int i=0; i<5;i++){
+//            code=code*10;
+//            code+= random.nextInt(0,9);
+//        }
+//        entity.setCode(String.valueOf(code));
+//
+//    }
 }
